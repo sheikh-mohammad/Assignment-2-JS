5c6952dc166239944dd9861332d9facc8d1e9cad
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-07 22:49:38 +0500

    "Added JavaScript code for number check with if-else statement"

---
+++ 1_If_Else_IF_Else/01_Number_Check/script.js
@@ -0,0 +1,9 @@
+var num = +prompt("Enter Any Number:");
+
+if (num > 0) {
+    console.log("Positive number");
+} else if (num < 0) {
+    console.log("Negative number");
+} else {
+    console.log("Zero");
+}
