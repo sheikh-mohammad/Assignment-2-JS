77b25aede83731fe22fd89d634c03f6e55a596a3
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-08 08:13:09 +0500

    "Added script.js with a marks grade system using if-else statements"

---
+++ 1_If_Else_IF_Else/04_Marks_Grade_System/script.js
@@ -0,0 +1,11 @@
+var marks = +prompt("Enter Your Marks:");
+
+if (marks >= 90) {
+    console.log("A");
+} else if (marks >= 80) {
+    console.log("B");
+} else if (marks >= 70) {
+    console.log("C");
+} else if (marks < 60) {
+    console.log("Fail");
+}
