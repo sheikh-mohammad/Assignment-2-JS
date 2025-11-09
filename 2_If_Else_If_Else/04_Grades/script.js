84f530337d76c0d23889c12158ce40f62b358d62
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-09 16:51:13 +0500

    `Added script.js file with grade calculator logic`

---
+++ 2_If_Else_If_Else/04_Grades/script.js
@@ -0,0 +1,11 @@
+var marks = +prompt("Enter Your Marks:");
+
+if (marks >= 90) {
+    console.log("A");
+} else if (marks >= 75) {
+    console.log("B");
+} else if (marks >= 50) {
+    console.log("C");
+} else if (marks < 50) {
+    console.log("Fail");
+}
