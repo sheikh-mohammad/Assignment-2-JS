055472f191bbb5b628a983a858de166600391793
Sheikh Mohammad <sheikhmohammadahmed07@gmail.com>
2025-11-09 17:16:04 +0500

    `Added script.js with if-else if-else statement to check number range`

---
+++ 2_If_Else_If_Else/08_Number_Range_Check/script.js
@@ -0,0 +1,11 @@
+var num = +prompt("Enter Any Number:");
+
+if (num <= 10) {
+    console.log("Between 1 and 10");
+} else if (num <= 20) {
+    console.log("Between 11 and 20");
+} else if (num <= 30) {
+    console.log("Between 21 and 30");
+} else {
+    console.log("Number is out of range");
+}
